59b1f01518d28e95cfa743d4ac99b5b66ca2a8c9
Oscar Peñaranda <DAM 6J@A019-PC14>
2018-05-10 11:54:26 +0200

    commit del PDF ejercicios 2B

---
+++ src/HolaMundo/HolaMundo.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package HolaMundo;
+
+/**
+ * 
+ *
+ * @author Óscar
+ * version 1.0
+ * @date 10-may-2018
+ */
+public class HolaMundo {
+    public static void main(String[] args) {
+        System.out.println("hola mundo");
+    }
+}
